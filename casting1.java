ae1602a54e8396263bf5209f12ea4a8645a09387
ivanivanov9 <72138590+ivanivanov9@users.noreply.github.com>
2020-09-30 15:19:02 +0300

    Add files via upload

---
+++ casting1.java
@@ -0,0 +1,23 @@
+package csting01;
+
+public class casting1 {
+
+	public casting1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//byte k =128;
+short i = 128;
+byte shortToByte = (byte)i;
+System.out.println(shortToByte);
+byte t = 127;
+t++;
+System.out.println(t);
+t++;
+System.out.println(t);
+
+	}
+
+}
---
+++ input04.java
@@ -0,0 +1,26 @@
+package input04kalataedition;
+
+import java.util.Scanner;
+
+public class input04 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(input04.class.getResourceAsStream("input04text.txt"));      
+        
+        //Edit these lines to advance the scanner
+        sc.nextLine();
+        sc.nextLine();
+        sc.nextLine();
+        
+        
+        //Does this line contain "BlueBumper"?
+        System.out.println(sc.findInLine("BlueBumper"));
+        //Store the next two numbers as xPosition and yPosition
+        //Print these positions
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        
+        
+        System.out.println("X: "    +x    +", Y: "    +y    );
+        sc.close();
+    }    
+}
