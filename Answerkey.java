ebb9dc9e48792c6c1ffbffbbbad7d06bc7f3b841
ivanivanov9 <72138590+ivanivanov9@users.noreply.github.com>
2020-10-21 11:35:40 +0300

    Add files via upload

---
+++ Answerkey.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Answerkey {
+
+	public Answerkey() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int NUMBER_STUDENTS = 3;
+		final int NUMBER_QUESTIONS = 3;
+		char[][] answers = new char[NUMBER_STUDENTS][NUMBER_QUESTIONS];
+		char[] keys = new char[NUMBER_QUESTIONS];
+		Scanner sc = new Scanner(System.in);
+		for (int j = 0; j < NUMBER_QUESTIONS; j++) {
+			System.out.println("Enter answer for question " + (j + 1));
+			keys[j] = sc.nextLine().charAt(0);
+		}
+		for (int i = 0; i < NUMBER_STUDENTS; i++) {
+			for (int j = 0; j < NUMBER_QUESTIONS; j++) {
+				System.out.println("Enter answer for question " + (j + 1) + " for student " + (i + 1));
+				answers[i][j] = sc.nextLine().charAt(0);
+			}
+		}
+		for (int i = 0; i < NUMBER_STUDENTS; i++) {
+			int correctAnswers = 0;
+			for (int j = 0; j < NUMBER_QUESTIONS; j++) {
+				if (answers[i][j] == keys[j]) {
+					correctAnswers++;
+				}
+				System.out.println("Student " + i + "'s correct answers count is " + correctAnswers);
+			}
+		}
+
+	}
+
+}
