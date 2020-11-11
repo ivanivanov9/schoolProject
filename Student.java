ac609092076f028aca559f5ae975448197144a2b
ivanivanov9 <72138590+ivanivanov9@users.noreply.github.com>
2020-11-11 11:59:15 +0200

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,14 @@
+
+public class Student {
+public String name;
+public double grade;
+
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+	public Student( String name, double grade) {
+		this.name = name;
+		this.grade = grade;
+	}
+
+}
