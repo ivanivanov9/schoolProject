87bc5fd38657c58250ce890866c976383a582510
ivanivanov9 <72138590+ivanivanov9@users.noreply.github.com>
2020-11-25 15:18:14 +0200

    Add files via upload

---
+++ Emirp.java
@@ -0,0 +1,13 @@
+
+public class Emirp {
+
+	public Emirp() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
